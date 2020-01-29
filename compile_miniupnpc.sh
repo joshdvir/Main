8ef9666f0ade67c97e2d5c2c571773a9b67b76d3
Josh Dvir <josh@dvir.us>
2020-01-29 09:02:45 -0500

    adding miniupnpc to poen port in routers

---
+++ compile_miniupnpc.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+TOOLCHAIN=$(pwd)/toolchain/bin
+CROSS_COMPILE=$TOOLCHAIN/mips-linux-gnu-
+export CC=${CROSS_COMPILE}gcc
+export LD=${CROSS_COMPILE}ld
+export CFLAGS="-muclibc -O2"
+export CPPFLAGS="-muclibc -O2"
+export LDFLAGS="-muclibc -O2"
+
+cd ./miniupnpc-2.1.20191224
+make clean
+make
